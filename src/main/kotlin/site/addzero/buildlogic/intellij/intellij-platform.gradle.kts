f44c97fb3d26fc4ed0be54ae4dcbce159fc5c4da
zjarlin <zjarlin@outlook.com>
2025-11-02 10:40:10 +0800

    change

--- src/main/kotlin/site/addzero/buildlogic/intellij/intellij-platform.gradle.kts
+++ src/main/kotlin/site/addzero/buildlogic/intellij/intellij-platform.gradle.kts
@@ -1,11 +1,27 @@
 package site.addzero.buildlogic.intellij
 
+import org.gradle.kotlin.dsl.named
+import org.jetbrains.changelog.markdownToHTML
+import org.jetbrains.intellij.platform.gradle.tasks.RunIdeTask
 
 
 plugins {
-    id("site.addzero.buildlogic.intellij.intellij-platform-core")
+    id("org.jetbrains.intellij.platform")
+    id("org.jetbrains.changelog")
+    id("site.addzero.buildlogic.jvm.kotlin-convention")
+
 
 }
+repositories {
+    mavenCentral()
+    intellijPlatform {
+        releases()
+        marketplace()
+        defaultRepositories()
+    }
+}
+
+
 
 dependencies {
     intellijPlatform {
@@ -18,5 +34,53 @@ dependencies {
 
 }
 
+fun String.md2html(): String {
+    val file = File(projectDir, this)
+    if (!file.exists()) {
+        file.parentFile.mkdirs()
+        file.createNewFile()
+    }
+    val run = file.readText().run {
+        markdownToHTML(this)
+    }
+    return run
+}
 
 
+
+
+
+
+intellijPlatform {
+    pluginConfiguration {
+        vendor {
+            name = "zjarlin"
+            email = "zjarlin@outlook.com"
+        }
+        description = "README.md".md2html()
+        changeNotes = "CHANGELOG.md".md2html()
+    }
+}
+
+
+tasks {
+    patchPluginXml {
+        sinceBuild.set("242")
+        untilBuild.set("262.*")
+    }
+    signPlugin {
+        certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
+        privateKey.set(System.getenv("PRIVATE_KEY"))
+        password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
+    }
+
+    publishPlugin {
+        token.set(System.getenv("PUBLISH_TOKEN"))
+    }
+}
+
+tasks.named<RunIdeTask>("runIde") {
+    jvmArgumentProviders += CommandLineArgumentProvider {
+        listOf("-Didea.kotlin.plugin.use.k2=true")
+    }
+}
