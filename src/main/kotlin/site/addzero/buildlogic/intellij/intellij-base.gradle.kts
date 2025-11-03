fbe95971382c37b8d1b443311bea8661b7ab4fbc
zjarlin <zjarlin@outlook.com>
2025-11-03 21:04:35 +0800

    change

--- src/main/kotlin/site/addzero/buildlogic/intellij/intellij-base.gradle.kts
+++ src/main/kotlin/site/addzero/buildlogic/intellij/intellij-base.gradle.kts
@@ -1,11 +1,13 @@
 package site.addzero.buildlogic.intellij
 
+import io.spring.gradle.dependencymanagement.org.eclipse.sisu.launch.SisuExtensions.local
 import org.gradle.kotlin.dsl.dependencies
+import org.jetbrains.intellij.platform.gradle.extensions.intellijPlatform
 
 
 plugins {
     id("org.jetbrains.intellij.platform.base")
-    id("org.jetbrains.intellij.platform.migration")
+//    id("org.jetbrains.intellij.platform.migration")
     id("site.addzero.buildlogic.jvm.kotlin-convention")
 //    id("site.addzero.buildlogic.intellij.intellij-repo")
 
@@ -23,8 +25,8 @@ repositories {
 
 dependencies {
     intellijPlatform {
-        local("/Applications/IntelliJ IDEA.app")
-//        intellijIdeaUltimate("2025.2.3")
+//        local("/Applications/IntelliJ IDEA.app")
+        intellijIdeaUltimate("2025.2.3")
         bundledPlugins(
             "com.intellij.java", "org.jetbrains.kotlin"
         )
