92ddde8fb49c2ba201054b1b6f3507a93e039e0a
zjarlin <zjarlin@outlook.com>
2025-11-03 11:21:17 +0800

    change

--- src/main/kotlin/site/addzero/buildlogic/jvm/java-convention.gradle.kts
+++ src/main/kotlin/site/addzero/buildlogic/jvm/java-convention.gradle.kts
@@ -14,3 +14,8 @@ configUtf8()
 configureJavaCompatibility(javaVersion)
 configJavaToolChain(javaVersion)
 configJunitPlatform()
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+}
